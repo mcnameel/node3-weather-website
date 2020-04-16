c1c18f90b70cbe0466a24fded71cf8284190bc17
Luc McNamee <lucas.mm@slalom.com>
2020-04-16 11:25:40 -0400

    Setup for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,16 +11,14 @@ weatherForm.addEventListener("submit", (e) => {
   messageTwo.textContent = "";
   const location = search.value;
 
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          return (messageOne.textContent = data.error);
-        }
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        return (messageOne.textContent = data.error);
+      }
 
-        messageOne.textContent = data.location;
-        messageTwo.textContent = data.forecast;
-      });
-    }
-  );
+      messageOne.textContent = data.location;
+      messageTwo.textContent = data.forecast;
+    });
+  });
 });
